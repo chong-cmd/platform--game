0da235e4d4d01542d8efba9a7ca7d706fa4f4a57
chong-cmd <zhu182677167702023@outlook.com>
2025-05-22 21:41:32 +0800

    Add files via upload

--- player.js
+++ player.js
@@ -1,7 +1,8 @@
 class Player {
     constructor() {
         this.element = document.getElementById('player');
-        this.isJumping = false;
+        this.jumpCount = 0;           // 当前跳跃次数
+        this.maxJump = 2;             // 最大跳跃次数（二段跳）
         this.jumpForce = 0;
         this.gravity = 1.2;
         this.position = {
@@ -13,24 +14,25 @@ class Player {
     }
 
     jump() {
-        if (!this.isJumping) {
-            this.isJumping = true;
+        if (this.jumpCount < this.maxJump) {
             this.jumpForce = 20;
+            this.jumpCount++;
             this.element.style.transform = 'translateY(-5px)';
         }
     }
 
     update() {
-        if (this.isJumping) {
+        if (this.jumpForce > 0 || this.position.y > 0) {
             this.jumpForce -= this.gravity;
             this.position.y += this.jumpForce;
-            
+
             if (this.position.y <= 0) {
                 this.position.y = 0;
-                this.isJumping = false;
+                this.jumpForce = 0;
+                this.jumpCount = 0; // 落地时重置跳跃次数
                 this.element.style.transform = 'translateY(0)';
             }
-            
+
             this.element.style.bottom = this.position.y + 'px';
         }
     }
@@ -40,7 +42,7 @@ class Player {
     }
 
     reset() {
-        this.isJumping = false;
+        this.jumpCount = 0;
         this.jumpForce = 0;
         this.position.y = 0;
         this.element.style.bottom = '0px';
